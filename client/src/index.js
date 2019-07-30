c6848fbd22966fc319dff53dcb8855736960807b
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-07-30 17:27:44 +0200

    Removing Redux Dev Tools

--- client/src/index.js
+++ client/src/index.js
@@ -20,8 +20,7 @@ const rootReducer = combineReducers({
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
