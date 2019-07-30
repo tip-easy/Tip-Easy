3877eb5461f7f367612e0f242cf79defeaffa842
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-07-30 12:29:22 +0200

    Redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Components
+import App from './App';
 
+// Middlewares
+import thunk from 'redux-thunk';
+
+// Reducers
+import {} from './Reducers';
+
+const rootReducer = combineReducers({
+
+});
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept()
+};
 
 // serviceWorker.unregister();
