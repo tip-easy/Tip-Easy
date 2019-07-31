431e662402fb41483c12c69759f40d7e0da64c02
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-07-31 19:41:30 +0200

    Readding Redux DevTools for development

--- client/src/index.js
+++ client/src/index.js
@@ -11,16 +11,35 @@ import App from './App';
 import thunk from 'redux-thunk';
 
 // Reducers
-import { LoginReducer } from './Reducers';
+import { BalanceReducer,
+  DepositReducer,
+  LoginReducer,
+  PaymentMethodReducer,
+  RegistrationReducer,
+  SelectTipReducer,
+  TipReceiverReducer,
+  TransactionReducer,
+  UserReducer,
+  WithdrawalReducer, } from './Reducers';
 
 const rootReducer = combineReducers({
-  LoginReducer
+  BalanceReducer,
+  DepositReducer,
+  LoginReducer,
+  PaymentMethodReducer,
+  RegistrationReducer,
+  SelectTipReducer,
+  TipReceiverReducer,
+  TransactionReducer,
+  UserReducer,
+  WithdrawalReducer,
 });
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
