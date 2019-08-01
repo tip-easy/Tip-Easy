517cc021923f030e35bd48602924538490cce905
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-08-01 09:15:26 +0200

    Adding Reset Password actionTypes and actions

--- client/src/Actions/UserActions.js
+++ client/src/Actions/UserActions.js
@@ -61,6 +61,39 @@ export const updateUserInfo = ( changes, token ) => dispatch => {
     })
 }
 
+export const resetPassword = ( changes, token ) => dispatch => {
+  // Check incoming data in `changes` for one of the provided parameters. If not, reject it.
+
+  requestObject = {
+    current_password: changes.current_password,
+    new_password: changes.new_password,
+    new_password_confirm: new_password_confirm,
+  }
+
+  dispatch({ 
+    type: types.RESETTING_PASSWORD_START 
+  })
+
+  return axios.put(`${URL}/api/me/reset-password`, { 
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${token}`,
+      }
+    }, requestObject)
+    .then(res => {
+      dispatch({ 
+        type: types.RESETTING_PASSWORD_SUCCESS,
+      })
+    })
+
+    .catch(error => {
+      dispatch({ 
+        type: types.RESETTING_PASSWORD_FAILURE,
+        payload: {error} 
+      })
+    })
+}
+
 export const deleteUser = ( token ) => dispatch => {
   dispatch({
     type: types.DELETING_USER_START
