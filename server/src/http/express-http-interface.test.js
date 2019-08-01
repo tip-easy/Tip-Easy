bba7c02b4e55b19ff1afbc4f9be7875f13d69373
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-08-02 00:24:47 +0100

    Create Express HTTP Interface Tests

---
+++ server/src/http/express-http-interface.test.js
@@ -0,0 +1,21 @@
+const expressHTTPInterface = require('./express-http-interface');
+
+describe('Express HTTP Interface', () => {
+  it('should return a function when invoked', () => {
+    expect(typeof expressHTTPInterface()).toBe('function');
+  });
+
+  it('should not break when not passed a Promise', () => {
+    expect(typeof expressHTTPInterface(null)).toBe('function');
+    expect(typeof expressHTTPInterface({})).toBe('function');
+    expect(typeof expressHTTPInterface(Function.prototype)).toBe('function');
+  });
+
+  it('should return null when the returned function is invoked', () => {
+    expect(expressHTTPInterface()()).toEqual(null);
+  });
+
+  it('should return not break when the returned function is not passed objects', () => {
+    expect(typeof expressHTTPInterface()(null)).toBe('object');
+  });
+});
