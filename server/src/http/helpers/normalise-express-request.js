0128d9381185bee55a105a82fc37093345d671e3
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-08-05 19:06:24 +0100

    Create Express Request Normaliser + Add Tests

---
+++ server/src/http/helpers/normalise-express-request.js
@@ -0,0 +1,32 @@
+function normaliseExpressRequest(request = {}) {
+  const hasRequiredProps = requestContainsExpressProperties(request);
+  
+  
+  if (request && hasRequiredProps) {
+    // Returns immutable request object
+    return Object.freeze({
+      method: request.method,
+      path: request.path,
+      headers: request.headers,
+      pathParams: request.params,
+      queryParams: request.query,
+      body: request.body
+    });
+  } else {
+    throw new Error('normaliseExpressRequest: An object containing Express request object properties is required');
+  }
+
+  function requestContainsExpressProperties(request) {
+    return (
+      'method' in request &&
+      'path' in request &&
+      'headers' in request &&
+      'params' in request &&
+      'query' in request &&
+      'body' in request
+    );
+  }
+}
+
+
+module.exports = normaliseExpressRequest;
