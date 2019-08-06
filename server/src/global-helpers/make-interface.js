00c16fb18ac9d3789777eac2704b4e6ff3546d15
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-08-06 09:43:50 +0100

    makeInterface: Rename "args" to "params"

--- server/src/global-helpers/make-interface.js
+++ server/src/global-helpers/make-interface.js
@@ -1,12 +1,13 @@
-function makeInterface({ interfaceFunction, ...dependencies } = {}, requiredArgs = []) {
-  requiredArgs.forEach(arg => {
-    if (!dependencies[arg]) {
-      throw new Error(`makeInterface: "${arg}" is a required dependency argument`);
+function makeInterface({ interfaceFunction, ...dependencies } = {}, requiredParams = []) {
+  requiredParams.forEach(param => {
+    if (!dependencies[param]) {
+      throw new Error(`makeInterface: "${param}" is a required dependency argument`);
     }
   });
 
   if (interfaceFunction && typeof interfaceFunction === 'function') {
-    const interfaceFunctionWithInjectedDependencies = () => interfaceFunction(dependencies);
+    const interfaceFunctionWithInjectedDependencies = (interfaceParams) => 
+      interfaceFunction({ ...dependencies, ...interfaceParams });
     return interfaceFunctionWithInjectedDependencies;
   } else {
     throw new Error('makeInterface: An interface function is required as a argument.');
