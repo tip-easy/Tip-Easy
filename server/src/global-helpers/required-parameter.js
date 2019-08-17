8736cee8056d72fa97d0974ba524e6be5e98dd85
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-08-17 01:30:06 +0100

    Add Todo to Required Parameter Function

--- server/src/global-helpers/required-parameter.js
+++ server/src/global-helpers/required-parameter.js
@@ -1,3 +1,4 @@
+// TODO: Change the message to be more suitable for any type of param not just named props
 function requiredParam(paramName) {
   const error = new Error(`"${paramName}" parameter cannot be null or undefined.`);
   error.type = 'Application';
