e7001c5aef457a4b03be674b313fa1371806117e
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-08-27 23:31:01 +0200

    RegistrationActionCreators

--- client/src/Actions/ActionCreators/RegistrationActionCreators.js
+++ client/src/Actions/ActionCreators/RegistrationActionCreators.js
@@ -1,1 +1,31 @@
 import * as types from '../actionTypes';
+
+export const registeringStart = () => {
+  return { 
+    type: types.REGISTERING_START 
+  }
+}
+
+export const registeringFailureInvalidParams = () => {
+  return { 
+    type: types.LOGGING_IN_FAILURE, 
+    payload: {
+      error: "Make sure you're passing a valid email and password, as well as the default currency!"
+    } 
+  }
+}
+
+export const registeringSuccess = () => {
+  return {
+    type: types.REGISTERING_SUCCESS
+  }
+}
+
+export const registeringFailure = (error) => {
+  return { 
+    type: types.REGISTERING_FAILURE, 
+    payload: {
+      error
+    } 
+  }
+}
--- client/src/Actions/RegistrationActions.js
+++ client/src/Actions/RegistrationActions.js
@@ -1,29 +1,20 @@
 import axios from 'axios';
 import { URL } from './index';
-import * as types from './actionTypes';
+import * as creators from './ActionCreators/RegistrationActionCreators';
 
 import { login } from './LoginActions';
 
 export const register = user_info => dispatch => {
-  dispatch({ 
-    type: types.REGISTERING_START 
-  });
+  dispatch(creators.registeringStart());
 
   const {email, password, default_currency} = user_info;
   if (!email || !password || typeof(email) !== "string" || typeof(password) !== "string" || password.length < 8 ) {
-    dispatch({ 
-      type: types.LOGGING_IN_FAILURE, 
-      payload: {
-        error: "Make sure you're passing a valid email and password, as well as the default currency!"
-      } 
-    });  
+    dispatch(creators.registeringFailureInvalidParams());  
   }
 
   return axios.post(`${URL}/api/register`, {email, password, default_currency})
     .then(res => {
-      dispatch({
-        type: types.REGISTERING_SUCCESS
-      })
+      dispatch(creators.registeringSuccess())
       
       // Found in LoginActions
       login({
@@ -33,11 +24,6 @@ export const register = user_info => dispatch => {
     })
     
     .catch(error => {
-      dispatch({ 
-        type: types.REGISTERING_FAILURE, 
-        payload: {
-          error
-        } 
-      });
+      dispatch(creators.registeringFailure(error));
     })
 };
