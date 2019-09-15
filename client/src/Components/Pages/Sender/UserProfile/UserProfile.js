c3e081bed7834229918d6288e078d2b3a264d03d
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-09-15 21:53:36 +0200

    Pushing changes end-of-day Sept 15.

--- client/src/Components/Pages/Sender/UserProfile/UserProfile.js
+++ client/src/Components/Pages/Sender/UserProfile/UserProfile.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 
 // Check for user account type
 // Shown data (all account types):
@@ -17,7 +18,16 @@ import { bindActionCreators } from 'redux';
 export const UserProfile = () => {
   return (
     <div>
-      UserProfile
+      <br/>UserProfile is used for both account types <br/>
+      After login, tip receivers will be redirected to this page. Tip senders can access it through the menu <br/><br/> 
+      
+      For tip receivers, this will show the receiver code<br/>
+      The basic info of the wallet is shown (i.e. current balance), but a separate button can be pressed to go to /user/wallet to see all the particulars and latest transactions<br/>
+      At the bottom of the page, there's a button with 'Send Tip' which then leads to the Sender Payment Flow
+
+      <br/><br/>
+      In the case of tip senders, it shows the current wallet balance, the last three tips sent, and basic account info like name<br/><br/>
+      <Link to="" ></Link>
     </div>
   )
 }
