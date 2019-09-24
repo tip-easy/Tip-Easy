61f833f2ee7dd90ab12949ec6ff267d19967952f
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-09-24 08:15:46 +0200

    Creating general file structure and lifting Authentication Flow

---
+++ client/src/Pages/WithdrawTips/OverviewWithdrawalAccounts.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+
+import BackButton from '../../Components/General/BackButton'
+
+export const OverviewWithdrawalAccounts = () => {
+  return (
+    <div>
+      <BackButton to="/user/settings" anchorText="Back To Settings" />
+      <h2>OverviewWithdrawalAccounts</h2>
+      <div>
+        <h4>Account 1 - 2222-1111-2222-1111</h4>
+        <p>Last used: Sep 9th</p>
+      </div>
+      <div>
+        <h4>Account 1 - 2222-1111-2222-1111</h4>
+        <p>Last used: Sep 9th</p>
+      </div>
+      <p>User can click 'Add Withdraw Method', which'll open up a Stripe window. Afterwards, they'll get taken to <Link to="/withdraw/accounts/success">Successfully Added Withdraw Method</Link></p>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(OverviewWithdrawalAccounts);
