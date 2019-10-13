7eb96503eb617537d5bddac9671914be889a3b44
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-10-14 00:32:42 +0100

    Create getUserByEmail DB Helper Function

--- server/src/database/auth-queries.js
+++ server/src/database/auth-queries.js
@@ -11,10 +11,33 @@ async function createUser(userObj) {
       }
       // Close Mongo client connection
       db.close();
+      console.log('db connection closed');
     });
   });
 }
 
+async function getUserByEmail(email) {
+  const [Users, db] = await getCollection('users');
+  return new Promise((resolve, reject) => {
+    Users.findOne(
+      { email },
+      // Return only _id field
+      { projection: { _id: 1 } },
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+        // Close Mongo client connection
+        db.close();
+        console.log('db connection closed');
+      }
+    );
+  });
+}
+
 module.exports = {
-  createUser
+  createUser,
+  getUserByEmail
 }
