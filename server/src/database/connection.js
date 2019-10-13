1dd33b84cfc26eaa5992ff1b7d20e3ef366dce25
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-10-14 00:14:14 +0100

    Change Connection File to Use getDBConnection Function

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -1,21 +1,28 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.DB_URI || 'mongodb://localhost:27017/';
-const dbClient = new MongoClient(uri, { useNewUrlParser: true });
+const dbClient = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ });
 
 // Mongo Node driver uses callbacks, I prefer Promises
 // Here we are wrapping the database connection method
 // in a Promise that resolves when connection is successful
-const dbConnection = new Promise((resolve, reject) => {
-  dbClient.connect((err, connection) => {
-    if (err) {
-      dbClient.close();
-      reject(err);
-    } else if (connection) {
-      resolve(connection)
-    }
+function getDbConnection() {
+  return new Promise((resolve, reject) => {
+    dbClient.connect((err, connection) => {
+      console.log('db connection opened');
+      if (err) {
+        dbClient.close();
+        console.log('db connection closed');
+        reject(err);
+      } else if (connection) {
+        resolve(connection);
+      }
+    });
   });
-})
+}
 
 // Export the client so that client connection can 
 // be closed elsewhere in the application
-module.exports = { dbConnection, dbClient };
+module.exports = { getDbConnection, dbClient };
--- server/src/database/get-collection.js
+++ server/src/database/get-collection.js
@@ -1,8 +1,9 @@
-const { dbConnection, dbClient } = require('./connection');
+const { getDbConnection, dbClient } = require('./connection');
 
+// TODO: Turn this function into a HOF. Usage: getCollection('dbname')('collection')
 async function getCollection(collection, db = 'tipeasyusers') {
   // Await successful db connection
-  const connection = await dbConnection;
+  const connection = await getDbConnection();
   // Return required collection & Mongo client instance
   return [connection.db(db).collection(collection), dbClient];
 }
