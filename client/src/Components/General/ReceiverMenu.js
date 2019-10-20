e96f0f9266aec580803ece950de4312581466834
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-20 10:26:15 +0200

    Workaround for dropdown menu for ReceiverMenu

--- client/src/Components/General/ReceiverMenu.js
+++ client/src/Components/General/ReceiverMenu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withRouter, Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -6,24 +6,45 @@ import { connect } from 'react-redux';
 import { logout } from '../../Actions/index';
 
 const ReceiverMenu = (props) => {
-    return (
-      <div>
-        <p>ReceiverMenu</p>
+  const [menuVisibility, setMenuVisibility] = useState(false)
 
-        <div>
-          {
-            props.location.pathname !== "/wallet" ?  
-              <Link to="/wallet">Wallet</Link>
+  return (
+    <>
+      <div>
+        <p 
+          // In lieu of a dropdown menu using <select>. Dropdown experience will be created through absolute positioning in styling.
+          onClick={() => setMenuVisibility(!menuVisibility)}
+        >
+          ReceiverMenu
+        </p>
+
+        { menuVisibility ?
+          <div>
+            {/* If user is currently in the wallet, there's no need to show the wallet button */}
+            { props.location.pathname !== "/wallet" ?  
+              <p><Link to="/wallet">Wallet</Link></p>
             : 
               null
-          }
-          <Link to="/withdraw">Withdraw Tips</Link>
-          <Link to="/tip/select-amount">Send Tips</Link>
-          <Link to="/user/settings">Settings</Link>
-        </div>
-        <p onClick={() => props.logout()}>Logout</p>
+            }
+            <p><Link to="/withdraw">Withdraw Tips</Link></p>
+            <p><Link to="/tip/select-amount">Send Tips</Link></p>
+            <p><Link to="/user/settings">Settings</Link></p>
+            <p onClick={() => props.logout()}>Logout</p>
+          </div>
+        :
+          null
+        }
+
+
+
+        
+
+
+
+
       </div>
-    )
+    </>
+  )
 };
 
 
